4711259a1841a93ba28b396f000e8494ee50aea2
fedor.belov <fbelov@gramant.ru>
2013-08-30 11:40:12 +0400

    removed unnecessary class

--- LivereloadGrailsPlugin.groovy
+++ LivereloadGrailsPlugin.groovy
@@ -11,7 +11,7 @@ class LivereloadGrailsPlugin {
     private static final Logger _log = LoggerFactory.getLogger("io.belov.grails.livereload.LivereloadGrailsPlugin")
 
     // the plugin version
-    def version = "0.2.10"
+    def version = "0.2.11"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
